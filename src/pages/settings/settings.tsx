61aa4f28d617785f995d49dbeb846b7947ec92b0
viizz29 <viizz29@gmail.com>
2026-06-25 09:26:05 +0000

    fixed the build errors

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper } from "@mui/material";
+import { Paper } from "@mui/material";
 import PageWrapper from "@/components/layouts/page-wrapper";
 
 export default function SettingsPage() {
